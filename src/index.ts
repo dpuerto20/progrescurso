3e145e24f63a84f872cd6c0356786207871f62d8
Àngel David <dpuerto666@gmail.com>
2021-02-21 11:10:48 -0800

    buevo cambio variable

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,7 @@ const english: Materia = {
 console.log(
   `la materia ${english.nombre} la dicta el profesor ${english.profesor.nombre} y esta dura solo  ${english.horario.length} dias  ${english.horario} con una intensidad de ${english.intensidad} horas  `
 );
+
+let perro 
+ 
+perro = a;

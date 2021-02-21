bde60155b117d720495981782577eaa9393c09da
Àngel David <dppuerto666@gmail.com>
2021-02-21 10:54:39 -0800

    ejercicio js

---
+++ src/ejercicios/01- tipo- Basicos .ts
@@ -0,0 +1,25 @@
+
+
+console.log('Hola Mundo!');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let  nombre: string  | number = 'strider'; 
+
+nombre = 'solo pruba ';
+ 
+let hp : number | string = 25 ;   
+let  estavivo : boolean = false;
+
+nombre = 77;
+hp = 'opera. gg',
+
+
+
+
+console.log ( nombre, hp );
+
+
+
